ec0448e34d97338611ed2d78495a84f5f8c09fef
Matt Gibson <mgibson5@mail.greenriver.edu>
2015-01-21 01:33:27 -0800

    version 2

---
+++ app.js
@@ -0,0 +1,60 @@
+var main = function () {
+    var toDos = ["list item 1",
+                 "list item 2",
+                 "list item 3",
+                 "list item 4",
+                 "list item 5",
+                 "list item 6"];
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                // newest first 
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // oldest first
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+    
+            }
+
+            $("main .content").append($content);
+
+            return false;
+        });
+    });
+
+    $(".tabs a:first-child span").trigger("click");
+};
+
+$(document).ready(main);
